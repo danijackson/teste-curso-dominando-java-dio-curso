67d9f975609a3c85eb73a7037c4b43a7ea13898d
Danijackson <daniassim@live.com>
2021-09-10 18:23:56 -0300

    First commit

---
+++ src/com/dio/Primeiroprograma.java
@@ -0,0 +1,17 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class Primeiroprograma {
+
+	public static void main(String[] args) {
+		Gato gato = new Gato();
+			Livro livro = new Livros ();
+	}
+
+}
+
+class Livros {
+	private String nome;
+	private String npag;
+}
